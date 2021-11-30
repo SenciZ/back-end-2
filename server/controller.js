9bdb78081b144cc809b8346213d60ce88640fb1a
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-30 14:16:01 -0700

    complete lab

---
+++ server/controller.js
@@ -0,0 +1,42 @@
+const houses = require('./db.json')
+let newID = 4;
+
+module.exports = {
+    getHouses: (req, res)=>{
+        res.status(200).send(houses)
+    },
+
+    deleteHouse: (req, res)=>{
+        const {id} = req.params;
+        let index = houses.findIndex(house => +house.id === +id)
+        houses.splice(index,1)
+        res.status(200).send(houses)
+    },
+
+    createHouse: (req,res)=>{
+        const {address,price, imageURL} = req.body;
+        const newHouse = {
+            address,
+            price,
+            imageURL,
+            id: newID
+        }
+        houses.push(newHouse)
+        newID++
+        res.status(200).send(houses)
+    },
+
+    updateHouse: (req,res)=>{
+        const {id} = req.params;
+        const {type} = req.body
+        const index = houses.findIndex(house => +house.id === +id)
+        if(type === "plus"){
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else if(type==='minus'){
+            houses[index].price -= 10000;
+            res.status(200).send(houses)
+
+        }
+    }
+}
